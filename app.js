a1d38d1639bdeef77d50e04bef165fc8843d564c
Mehdi Benadda <me@mbenadda.com>
2018-11-22 18:08:30 +0100

    🎨(format) prettierify all relevant files in the project
    Contents of this repository were not following standards style
    rules or linter configurations. We simply added a light
    .prettierrc file and ran prettier over all .js and .json files
    in the repository.
    
    This should help us keep the code tidy and minimize diffs as
    we maintain the library in the future.

--- app.js
+++ app.js
@@ -4,67 +4,67 @@ var path = require('path');
 var subsrt = require('./lib/subsrt.js');
 
 var config = {
-  verbose: process.env.NODE_VERBOSE == "true" || process.env.NODE_VERBOSE == "1"
+  verbose:
+    process.env.NODE_VERBOSE == 'true' || process.env.NODE_VERBOSE == '1',
 };
 
-//Command line arguments
+// Command line arguments
 var args = process.argv.slice(2);
 for (var i = 0; i < args.length; i++) {
   switch (args[i]) {
-    case "list":
-    case "parse":
-    case "build":
-    case "detect":
-    case "resync":
-    case "convert":
+    case 'list':
+    case 'parse':
+    case 'build':
+    case 'detect':
+    case 'resync':
+    case 'convert':
       if (config.command) {
-        console.error("Cannot run more than one command: " + args[i]);
+        console.error('Cannot run more than one command: ' + args[i]);
         process.exit(1);
       }
       config.command = args[i];
       break;
-      
-    case "--eol":
+
+    case '--eol':
       config.eol = args[++i];
       if (config.eol) {
-        config.eol = config.eol.replace(/\\r/g, "\r").replace(/\\n/g, "\n");
+        config.eol = config.eol.replace(/\\r/g, '\r').replace(/\\n/g, '\n');
       }
       break;
-      
-    case "--fps":
+
+    case '--fps':
       var fps = args[++i];
-      if (fps.indexOf("-") > 0) {
-        fps = fps.split("-");
+      if (fps.indexOf('-') > 0) {
+        fps = fps.split('-');
         config.fpsFrom = parseFloat(fps[0]);
         config.fpsTo = parseFloat(fps[1]);
-      }
-      else {
+      } else {
         config.fps = parseFloat(fps);
       }
       break;
-      
-    case "--offset":
+
+    case '--offset':
       config.offset = parseInt(args[++i]);
       break;
-      
-    case "--format":
+
+    case '--format':
       config.format = args[++i];
       break;
-      
-    case "--help":
+
+    case '--help':
       help();
       process.exit(0);
       break;
-      
-    case "--verbose":
+
+    case '--verbose':
       config.verbose = true;
       break;
-      
-    case "--version":
+
+    case '--version':
       console.log(require('./package.json').version);
       process.exit(0);
       break;
-      
+
     default:
       if (!config.src) {
         config.src = args[i];
@@ -74,67 +74,70 @@ for (var i = 0; i < args.length; i++) {
         config.dst = args[i];
         continue;
       }
-      console.error("Unknown command line argument: " + args[i]);
+      console.error('Unknown command line argument: ' + args[i]);
       process.exit(1);
       break;
   }
 }
 
-//Prints help message
+// Prints help message
 function help() {
-  console.log("Usage:");
-  console.log("  subsrt [command] [options]");
-  console.log("");
-  console.log("Commands:");
-  console.log("  list                   List supported formats");
-  console.log("  parse [src] [json]     Parse a subtitle file");
-  console.log("  build [json] [dst]     Create a subtitle file from captions");
-  console.log("  detect [src]           Detect subtitle file format, if supported");
-  console.log("  resync [src] [dst]     Resync FPS or shift time (+/- offset)");
-  console.log("  convert [src] [dst]    Converts a subtitle format");
-  console.log("");
-  console.log("Options:");
-  console.log("  --help                 Print this message");
-  console.log("  --eol [chars]          End of line chars, e.g. \\r\\n");
-  console.log("  --fps [fps]            Frames per second for .sub format");
-  console.log("  --offset [time]        Resync time shift offset in ms");
-  console.log("  --format [ext]         Subtitle format to convert/build/parse");
-  console.log("  --verbose              Enable detailed logging");
-  console.log("  --version              Print version number");
-  console.log("");
-  console.log("Examples:");
-  console.log("  subsrt parse sample.sbv");
-  console.log("  subsrt parse sample.srt output.json");
-  console.log("  subsrt parse sample.sub --fps 30");
-  console.log("  subsrt build input.json output.vtt");
-  console.log("  subsrt build input.json --format sbv");
-  console.log("  subsrt detect unknown.txt");
-  console.log("  subsrt convert sample.srt sample.vtt");
-  console.log("  subsrt convert --offset -250 sample.srt sample.ssa");
-  console.log("  subsrt resync --offset +3000 input.srt output.srt");
-  console.log("  subsrt resync --fps 25-30 input.sub output.sub");
+  console.log('Usage:');
+  console.log('  subsrt [command] [options]');
+  console.log('');
+  console.log('Commands:');
+  console.log('  list                   List supported formats');
+  console.log('  parse [src] [json]     Parse a subtitle file');
+  console.log('  build [json] [dst]     Create a subtitle file from captions');
+  console.log(
+    '  detect [src]           Detect subtitle file format, if supported',
+  );
+  console.log('  resync [src] [dst]     Resync FPS or shift time (+/- offset)');
+  console.log('  convert [src] [dst]    Converts a subtitle format');
+  console.log('');
+  console.log('Options:');
+  console.log('  --help                 Print this message');
+  console.log('  --eol [chars]          End of line chars, e.g. \\r\\n');
+  console.log('  --fps [fps]            Frames per second for .sub format');
+  console.log('  --offset [time]        Resync time shift offset in ms');
+  console.log(
+    '  --format [ext]         Subtitle format to convert/build/parse',
+  );
+  console.log('  --verbose              Enable detailed logging');
+  console.log('  --version              Print version number');
+  console.log('');
+  console.log('Examples:');
+  console.log('  subsrt parse sample.sbv');
+  console.log('  subsrt parse sample.srt output.json');
+  console.log('  subsrt parse sample.sub --fps 30');
+  console.log('  subsrt build input.json output.vtt');
+  console.log('  subsrt build input.json --format sbv');
+  console.log('  subsrt detect unknown.txt');
+  console.log('  subsrt convert sample.srt sample.vtt');
+  console.log('  subsrt convert --offset -250 sample.srt sample.ssa');
+  console.log('  subsrt resync --offset +3000 input.srt output.srt');
+  console.log('  subsrt resync --fps 25-30 input.sub output.sub');
 }
 
 var commands = {
   list: function() {
-    console.log(subsrt.list().join(", "));
+    console.log(subsrt.list().join(', '));
   },
   parse: function() {
     var content = fs.readFileSync(config.src, 'utf8');
-    
+
     var options = {
-      verbose: config.verbose
+      verbose: config.verbose,
     };
     if (config.fps) {
       options.fps = config.fps;
     }
-    
+
     var captions = subsrt.parse(content, options);
-    var json = JSON.stringify(captions, " ", 2);
+    var json = JSON.stringify(captions, ' ', 2);
     if (config.dst) {
       fs.writeFileSync(config.dst, json);
-    }
-    else {
+    } else {
       console.log(json);
     }
   },
@@ -143,12 +146,12 @@ var commands = {
     var captions = JSON.parse(json);
     if (!config.format && config.dst) {
       var ext = path.extname(config.dst);
-      config.format = ext.replace(/\./, "").toLowerCase();
+      config.format = ext.replace(/\./, '').toLowerCase();
     }
-    
+
     var options = {
       verbose: config.verbose,
-      format: config.format
+      format: config.format,
     };
     if (config.fps) {
       options.fps = config.fps;
@@ -156,22 +159,21 @@ var commands = {
     if (config.eol) {
       options.eol = config.eol;
     }
-    
+
     var content = subsrt.build(captions, options);
     if (config.dst) {
       fs.writeFileSync(config.dst, content);
-    }
-    else {
+    } else {
       console.log(content);
     }
   },
   detect: function() {
     var content = fs.readFileSync(config.src, 'utf8');
     var format = subsrt.detect(content);
-    console.log(format || "unknown");
+    console.log(format || 'unknown');
   },
   resync: function() {
-    var options = { };
+    var options = {};
     if (config.offset) {
       options.offset = config.offset;
     }
@@ -193,12 +195,12 @@ var commands = {
     var content = fs.readFileSync(config.src, 'utf8');
     if (!config.format && config.dst) {
       var ext = path.extname(config.dst);
-      config.format = ext.replace(/\./, "").toLowerCase();
+      config.format = ext.replace(/\./, '').toLowerCase();
     }
-    
+
     var options = {
       verbose: config.verbose,
-      format: config.format
+      format: config.format,
     };
     if (config.fps) {
       options.fps = config.fps;
@@ -209,21 +211,19 @@ var commands = {
     if (config.resync) {
       options.resync = config.resync;
     }
-    
+
     var converted = subsrt.convert(content, options);
     if (config.dst) {
       fs.writeFileSync(config.dst, converted);
-    }
-    else {
+    } else {
       console.log(converted);
     }
-  }
+  },
 };
 
 var func = commands[config.command];
-if (typeof func == "function") {
+if (typeof func == 'function') {
   func();
-}
-else {
+} else {
   help();
 }
